dd63b22251c089967d5030287404f06fd724d7fa
jindra12 <jindra12.underdark@gmail.com>
2023-01-12 19:02:35 +0100

    Create hello world in React

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,9 @@
-import "../scss/main.scss";
-console.log("Hello world");
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>
+        <h1>Hello, World</h1>
+    </div>,
+  document.getElementById("root"),
+);
